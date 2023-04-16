56ce63d8b7ca1b78f688c57aaa447c57fbd9620c
remish-nastya-0601 <91561043+remish-nastya-0601@users.noreply.github.com>
2023-04-16 17:14:08 +0300

    Add files via upload
    3.	Реализовать шифрование методом перестановки на Java. Алгоритм работы: входные данные - строка, ключ - целое число. Выходные данные - зашифрованная строка

---
+++ ShifrPerestanovka/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShifrPerestanovka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.OutputEncoding = Encoding.Unicode;
+                    Console.InputEncoding = Encoding.Unicode;
+                    Console.Write("Введите сообщение: ");
+                    string stroka = Console.ReadLine();
+                    while (stroka == "")
+                    {
+                        Console.WriteLine("Было введено пустое сообщение!");
+                        Console.Write("Введите сообщение: ");
+                        stroka = Console.ReadLine();
+                    }
+                    Console.Write("Введите ключ: ");
+                    string key_str = Console.ReadLine();
+                    while (key_str == "")
+                    {
+                        Console.WriteLine("Было введено пустое сообщение!");
+                        Console.Write("Введите сообщение: ");
+                        key_str = Console.ReadLine();
+                    }
+                    int key;
+                    bool is_key = true;
+                    string dist_key_str = string.Join("", key_str.Distinct().ToArray());
+                    //проверка на правильность ввода ключа
+                    if (!Int32.TryParse(key_str, out key) ||!dist_key_str.Equals(key_str)) is_key = false;
+                    else
+                    {
+                        for(int i=1; i <= key_str.Length; i++)
+                        {
+                            if(key_str.IndexOf(i.ToString()) == -1)
+                            {
+                                is_key = false;
+                                break;
+                            }
+                        }
+                    }
+
+                    while (!is_key)
+                    {
+                        Console.Write("Ключ должен быть целочисленным числом, содержащем перемешанную последовательность чисел от 1!\nВведите ключ: ");
+                        key_str = Console.ReadLine();
+                        dist_key_str = string.Join("", key_str.Distinct().ToArray());
+                        is_key = true;
+                        if (!Int32.TryParse(key_str, out key) || !dist_key_str.Equals(key_str)) is_key = false;
+                        else
+                        {                          
+                            for (int i = 1; i <= key_str.Length; i++)
+                            {
+                                if (key_str.IndexOf(i.ToString()) == -1)
+                                {
+                                    is_key = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+
+                    int array_strings = (int)Math.Ceiling(stroka.Length * 1.0 / key_str.Length);
+                    string[,] array = new string[array_strings, key_str.Length];
+                    string res = "";
+                    for(int i = 0; i< array_strings; i++)
+                    {
+                        for(int j=0; j< key_str.Length; j++)
+                        {
+                            if (i * key_str.Length + j < stroka.Length)
+                                array[i, j] = stroka[i * key_str.Length + j].ToString();
+                            else array[i, j] = "";
+                        }
+                    }
+
+                    for (int i = 0; i < key_str.Length; i++)
+                    {
+                        for (int j = 0; j < array_strings; j++)
+                        {                           
+                            res += array[j, key_str.IndexOf((i + 1).ToString())]; //2134
+                        }
+                    }
+                    Console.WriteLine("Зашифрованная строка: " + res + "\n");
+                    Console.ReadKey();
+                }
+                catch
+                {
+                    Console.WriteLine("Неправильный формат данных!\n");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ ShifrPerestanovka/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ShifrPerestanovka")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("ShifrPerestanovka")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("40eb0699-459b-4a86-a6a9-6c9af01ae427")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
