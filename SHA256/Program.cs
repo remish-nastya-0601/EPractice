7f208ae9d76b5a70d47009a241cde1acdbefb688
remish-nastya-0601 <91561043+remish-nastya-0601@users.noreply.github.com>
2023-04-16 17:20:14 +0300

    Add files via upload
    22.	Реализовать алгоритм хэширования SHA-256 на Java. Алгоритм работы: входные данные - сообщение. Выходные данные - хэш-значение.

---
+++ SHA256/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHA256
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    string stroka = "";
+                    Console.Write("Введите сообщение: ");
+                    stroka = Console.ReadLine();
+                    while (stroka == "")
+                    {
+                        Console.WriteLine("Было введено пустое сообщение!");
+                        Console.Write("Введите сообщение: ");
+                        stroka = Console.ReadLine();
+                    }   
+
+                    string res = Sha256(stroka);
+                    Console.WriteLine("Хэшированное сообщение: " + res + "\n");
+                }
+                catch
+                {
+                    Console.WriteLine("Неправильный формат данных!\n");
+                }
+            }
+        }
+
+        public static string Sha256(string message)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(message);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = string.Empty;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            return hashString;
+        }
+    }
+}
