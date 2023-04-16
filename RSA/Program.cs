3420c73a4d17c9243fea2334266a9b787f8db8cd
remish-nastya-0601 <91561043+remish-nastya-0601@users.noreply.github.com>
2023-04-16 17:57:43 +0300

    Add files via upload

---
+++ RSA/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            while (true)
+            {
+                Random rand = new Random();
+
+                Console.WriteLine("Введите открытый ключ: (два числа)");
+                Console.WriteLine("Первое число:");
+                int e = int.Parse(Console.ReadLine());
+                Console.WriteLine("Второе число:");
+                int N = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Открытый ключ: (" + e + ", " + N + ")");
+                
+                try
+                {
+                    Console.WriteLine("Введите сообщение:");
+                    string text = Console.ReadLine().ToLower();
+                    int mes = 0;
+                    while(!Int32.TryParse(text, out mes))
+                    {
+                        Console.WriteLine("Можно вводить только целые числа! Введите сообщение:");
+                        text = Console.ReadLine().ToLower();
+                    }
+                    int res_encode = (int)BigInteger.ModPow(mes, e, N);
+                    Console.WriteLine("Зашифрованное сообщение:\n" + res_encode);
+
+                }
+                catch
+                {
+                    Console.WriteLine("Неверный формат!");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
