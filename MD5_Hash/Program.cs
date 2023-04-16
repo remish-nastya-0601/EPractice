2f8bdcd9af2ce171d239692dd74019a973a801be
remish-nastya-0601 <91561043+remish-nastya-0601@users.noreply.github.com>
2023-04-16 17:26:23 +0300

    Add files via upload

---
+++ MD5_Hash/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MD5_Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введите сообщение: ");
+                    string stroka = Console.ReadLine();
+
+                    string res = CreateMD5(stroka);
+                    Console.WriteLine("Хэшированное сообщение: " + res + "\n");
+                }
+                catch
+                {
+                    Console.WriteLine("Неправильный формат данных!\n");
+                }
+            }
+        }
+
+        public static string CreateMD5(string message)
+        {
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] messageBytes = System.Text.Encoding.ASCII.GetBytes(message);
+                byte[] hashBytes = md5.ComputeHash(messageBytes);
+
+                 StringBuilder sb = new System.Text.StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
